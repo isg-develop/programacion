206d7fa69ac6476a189cfdeacbe03d878cdfb2ff
uth-tics <uth.tics.tareas@gmail.com>
2015-10-28 10:13:29 -0600

    reimplementacion

--- java/aplicacion/Practicas/src/tareas/Modulo2Tarea2.java
+++ java/aplicacion/Practicas/src/tareas/Modulo2Tarea2.java
@@ -1,12 +1,20 @@
 package tareas;
 import java.util.Scanner;
 public class Modulo2Tarea2 {
- 
-	public static void main(String[] ar){
-		
-		Scanner teclado = new Scanner(System.in);
+    private Scanner teclado;
+	public Modulo2Tarea2(){
+		// constructor		
+	}
+	
+	public void imprmirNumero(){
+		teclado = new Scanner(System.in);
 		System.out.print("Ingrese primer valor: ");
 		int numero = teclado.nextInt();
-		System.out.print("El numero es: " + numero);
+		System.out.print("El numero es: " + numero);		
+	}
+	
+	public static void main(String[] ar){
+		Modulo2Tarea2 objeto = new Modulo2Tarea2();
+		objeto.imprmirNumero();		
 	}
 }
