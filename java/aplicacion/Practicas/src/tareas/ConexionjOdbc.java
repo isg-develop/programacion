3a63039642a5217a160f5819f2a1404ae58d06de
uth-tics <uth.tics.tareas@gmail.com>
2015-10-28 11:24:20 -0600

    Conexion con postgreSQL

---
+++ java/aplicacion/Practicas/src/tareas/ConexionjOdbc.java
@@ -0,0 +1,59 @@
+package tareas;
+import java.sql.*;
+
+public class ConexionjOdbc {
+	
+	    private Connection conexion;
+	    private String basedatos = "BaseDemo";
+	    private String url = "jdbc:postgresql://localhost:5432/"+basedatos;
+	    private String driver = "org.postgresql.Driver";
+	    private String user = "postgres";
+	    private String pass = "s0porte";
+	    private Statement stmt = null;
+
+	    public ConexionjOdbc() {
+	    }
+	    
+	    public Connection conectar() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	        conexion = null;
+	        try {
+	            Class.forName(driver).newInstance();
+	            conexion = (DriverManager.getConnection(url, user, pass));
+	            if (conexion != null) {
+	              //System.out.println("SE HA ESTABLECIDO UNA CONEXION A LA BASE DE DATOS: " );
+	              //System.out.println("CON "+ url.toUpperCase());
+	            }
+	        } catch (SQLException e) {
+	            System.out.println(" Falla de conectividad:  " + " " + conexion);
+	            e.printStackTrace();
+	        }
+	        return conexion;
+	    }
+	    
+	    public Connection getConexion() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	        conexion = this.conectar();
+	        return conexion;
+	    }
+	    
+	    public void printConexion(){
+	    	try {
+	           if (conexion != null) {
+	              System.out.println("SE HA ESTABLECIDO UNA CONEXION A LA BASE DE DATOS: " );
+	              System.out.println("driver: "+ conexion.toString());
+	            }
+	        } catch (Exception e) {;}	    	
+	    }
+	     
+	    public Statement getStmt() {
+	        return stmt;
+	    }
+
+	    public void closeConexion() throws SQLException {
+	        conexion.close();
+	        conexion = null;
+	    }
+
+	    public void setStmt(Statement stmt) {
+	        this.stmt = stmt;
+	    }
+}
--- java/aplicacion/Practicas/src/tareas/Modulo2Tarea2.java
+++ java/aplicacion/Practicas/src/tareas/Modulo2Tarea2.java
@@ -1,4 +1,5 @@
 package tareas;
+import java.sql.*;
 import java.util.Scanner;
 public class Modulo2Tarea2 {
     private Scanner teclado;
@@ -10,11 +11,41 @@ public class Modulo2Tarea2 {
 		teclado = new Scanner(System.in);
 		System.out.print("Ingrese primer valor: ");
 		int numero = teclado.nextInt();
-		System.out.print("El numero es: " + numero);		
+		System.out.println("El numero es: " + numero);		
+	}
+	
+	public void GetEstudiantes(){
+		ConexionjOdbc baseDatos = new ConexionjOdbc();
+		Connection conexion = null;
+		ResultSet rs = null;
+		try {
+			conexion = baseDatos.getConexion();
+		
+			baseDatos.printConexion();
+            String sentencia ="SELECT id, nombre, matricula  FROM public.estudiantes; "; //"SELECT nombre,pass,rol FROM ct_empleados  WHERE (usuario = ?) AND (pass=?)";
+            PreparedStatement s = conexion.prepareStatement(sentencia);
+            //s.setString(1, nombreColumna); //parametro
+            rs = s.executeQuery();//ejecuta la consulta
+            if (rs != null) {
+                while (rs.next()) {
+                	System.out.println("-----------------------");
+                	System.out.println("id: "+rs.getInt("id") );
+                	System.out.println("nombre: "+rs.getString("nombre") );
+                	System.out.println("matricula: "+rs.getString("matricula") );
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(" Error by: " + e.getMessage());
+        }
+		finally {
+			try {
+       	        conexion.close(); //Cierra la conexion
+			} catch (Exception e) {  }
+		}
 	}
 	
 	public static void main(String[] ar){
 		Modulo2Tarea2 objeto = new Modulo2Tarea2();
-		objeto.imprmirNumero();		
+		objeto.GetEstudiantes();		
 	}
 }
