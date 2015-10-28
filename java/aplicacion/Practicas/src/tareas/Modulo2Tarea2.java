b0bbe26acdcfc6c1c72f8867c8c59d7bca42273a
uth-invitado <svn@cbtis08estatal.cu.cc>
2015-10-28 09:20:57 -0600

    Nuevo clase
    Demostracion

---
+++ java/aplicacion/Practicas/src/tareas/Modulo2Tarea2.java
@@ -0,0 +1,12 @@
+package tareas;
+import java.util.Scanner;
+public class Modulo2Tarea2 {
+ 
+	public static void main(String[] ar){
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Ingrese primer valor: ");
+		int numero = teclado.nextInt();
+		System.out.print("El numero es: " + numero);
+	}
+}
